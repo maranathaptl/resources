be7b9008dca575e2c79d23e32881bf8eb9fccc9e
Matteu <matteugt@gmail.com>
2024-04-30 04:47:05 +0800

    Setup stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,53 +1,27 @@
-<script>
-	import Header from './Header.svelte';
-	import './styles.css';
+<script lang="ts">
 </script>
 
 <div class="app">
-	<Header />
 
-	<main>
+	<main class="content-wrap">
 		<slot />
 	</main>
 
 	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+
 	</footer>
 </div>
 
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
+<style lang="scss">
+@use '@matteusan/himig';
+@use '@matteusan/himig/ms-button';
+@use '@matteusan/himig/ms-utils';
 
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
+@include himig.init();
 
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
+@include ms-button.render();
 
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
+@include ms-utils.layout();
+@include ms-utils.struct();
+@include ms-utils.typography();
 </style>
