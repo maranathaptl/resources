73d4ffe66b002cc0d935e09264af942b150f95f1
Matteu <matteugt@gmail.com>
2024-05-03 22:35:50 +0800

    Remove purgecss

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import purge from '@erbelion/vite-plugin-sveltekit-purgecss';
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
-		// @ts-ignore
-		purge(),
 	]
 });
