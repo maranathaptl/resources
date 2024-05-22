e0f9b85df5f88d34d27ca739a4a708a976de6859
Matteu <matteugt@gmail.com>
2024-05-23 04:48:21 +0800

    Delete some files

--- src/lib/db.ts
+++
@@ -1,7 +0,0 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
-import postgres from 'postgres';
-
-const connectionString = 'postgres://postgres.bpvjtftxvdwuxaiutktg:MaranathaResources@aws-0-us-west-1.pooler.supabase.com:5432/postgres';
-
-export const client = postgres(connectionString, { prepare: false })
-export const db = drizzle(client);
--- src/lib/schema.ts
+++
@@ -1,9 +0,0 @@
-import { date, json, pgTable, serial, varchar } from "drizzle-orm/pg-core";
-
-export const sheet_music = pgTable('sheet_music', {
-  id: serial('id').primaryKey(),
-  piece: varchar('piece'),
-  slug: varchar('slug'),
-  files: json('files'),
-  created_at: date('created_at'),
-});
