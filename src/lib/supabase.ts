80172ef3349d183a08c72903a623bf5c95265007
Matteu <matteugt@gmail.com>
2025-03-30 01:22:24 +0800

    Delete dead code

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,11 +4,3 @@ export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_KEY
 );
-
-export const sheetMusic = {
-  async all() {
-    const { data, error } = await supabase.from('sheet_music').select('*');
-    if (error) throw error;
-    return data;
-  }
-}
